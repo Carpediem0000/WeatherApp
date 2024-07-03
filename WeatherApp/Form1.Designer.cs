c6d2a31cb254f1f35fcf44ad3ff3abbc279b80ec
Ваня Зиновчук <zinovchukdfuz2@gmail.com>
2024-07-03 09:45:24 +0300

    Add project files.

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_sity = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_City = new System.Windows.Forms.Label();
+            this.lbl_Temp = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_CityName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_sity
+            // 
+            this.cb_sity.FormattingEnabled = true;
+            this.cb_sity.Location = new System.Drawing.Point(12, 41);
+            this.cb_sity.Name = "cb_sity";
+            this.cb_sity.Size = new System.Drawing.Size(121, 21);
+            this.cb_sity.TabIndex = 0;
+            this.cb_sity.SelectedIndexChanged += new System.EventHandler(this.cb_sity_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выбрать город";
+            // 
+            // lbl_City
+            // 
+            this.lbl_City.AutoSize = true;
+            this.lbl_City.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_City.Location = new System.Drawing.Point(48, 36);
+            this.lbl_City.Name = "lbl_City";
+            this.lbl_City.Size = new System.Drawing.Size(0, 24);
+            this.lbl_City.TabIndex = 2;
+            // 
+            // lbl_Temp
+            // 
+            this.lbl_Temp.AutoSize = true;
+            this.lbl_Temp.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_Temp.Location = new System.Drawing.Point(47, 120);
+            this.lbl_Temp.Name = "lbl_Temp";
+            this.lbl_Temp.Size = new System.Drawing.Size(19, 29);
+            this.lbl_Temp.TabIndex = 3;
+            this.lbl_Temp.Text = ".";
+            this.lbl_Temp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WeatherApp.Properties.Resources.free_icon_celsius_1274256;
+            this.pictureBox1.Location = new System.Drawing.Point(86, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_CityName
+            // 
+            this.lbl_CityName.AutoSize = true;
+            this.lbl_CityName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_CityName.Location = new System.Drawing.Point(253, 68);
+            this.lbl_CityName.Name = "lbl_CityName";
+            this.lbl_CityName.Size = new System.Drawing.Size(86, 31);
+            this.lbl_CityName.TabIndex = 5;
+            this.lbl_CityName.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 374);
+            this.Controls.Add(this.lbl_CityName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_Temp);
+            this.Controls.Add(this.lbl_City);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_sity);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_sity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_City;
+        private System.Windows.Forms.Label lbl_Temp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_CityName;
+    }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using TelegramBot_Dz;
+using static WeatherApp.Form1;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        private List<City> _citys;
+        public class City
+        {
+            public string Name { get; set; }
+            public string Region { get; set; }
+        }
+        public class IpInfo
+        {
+            public string Ip { get; set; }
+            public string Hostname { get; set; }
+            public string City { get; set; }
+            public string Region { get; set; }
+            public string Country { get; set; }
+            public string Loc { get; set; }
+            public string Org { get; set; }
+            public string Postal { get; set; }
+            public string Timezone { get; set; }
+        }
+        public static List<City> GetUkrainianRegionalCenters()
+        {
+            return new List<City>
+        {
+            new City { Name = "Kyiv", Region = "Kyiv" },
+            new City { Name = "Vinnytsia", Region = "Vinnytsia Oblast" },
+            new City { Name = "Dnipro", Region = "Dnipropetrovsk Oblast" },
+            new City { Name = "Donetsk", Region = "Donetsk Oblast" },
+            new City { Name = "Zhytomyr", Region = "Zhytomyr Oblast" },
+            new City { Name = "Zaporizhzhia", Region = "Zaporizhzhia Oblast" },
+            new City { Name = "Ivano-Frankivsk", Region = "Ivano-Frankivsk Oblast" },
+            new City { Name = "Kropyvnytskyi", Region = "Kirovohrad Oblast" },
+            new City { Name = "Luhansk", Region = "Luhansk Oblast" },
+            new City { Name = "Lviv", Region = "Lviv Oblast" },
+            new City { Name = "Mykolaiv", Region = "Mykolaiv Oblast" },
+            new City { Name = "Odesa", Region = "Odesa Oblast" },
+            new City { Name = "Poltava", Region = "Poltava Oblast" },
+            new City { Name = "Rivne", Region = "Rivne Oblast" },
+            new City { Name = "Sumy", Region = "Sumy Oblast" },
+            new City { Name = "Ternopil", Region = "Ternopil Oblast" },
+            new City { Name = "Kharkiv", Region = "Kharkiv Oblast" },
+            new City { Name = "Kherson", Region = "Kherson Oblast" },
+            new City { Name = "Khmelnytskyi", Region = "Khmelnytskyi Oblast" },
+            new City { Name = "Cherkasy", Region = "Cherkasy Oblast" },
+            new City { Name = "Chernivtsi", Region = "Chernivtsi Oblast" },
+            new City { Name = "Chernihiv", Region = "Chernihiv Oblast" }
+        };
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            _citys = GetUkrainianRegionalCenters();
+            foreach (var item in _citys)
+            {
+                cb_sity.Items.Add(item.Name);
+            }
+            cb_sity.SelectedIndex = 0;
+            lbl_CityName.Text = cb_sity.Items[cb_sity.SelectedIndex].ToString();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            string pubIp = new System.Net.WebClient().DownloadString("https://api.ipify.org"); // Внешний IP
+
+            var response = await httpClient.GetStringAsync($"https://ipinfo.io/{pubIp}/geo");
+            //XmlDocument res = JsonConvert.DeserializeXmlNode(response);
+            //MessageBox.Show(res.GetElementsByTagName("city_name").Item(0).Value);
+
+            IpInfo ipInfo = null;
+            if (response != null)
+            {
+                ipInfo = JsonConvert.DeserializeObject<IpInfo>(response);
+            }
+            //MessageBox.Show($"City: {ipInfo.City}");
+            for (int i = 0; i < cb_sity.Items.Count; i++)
+            {
+                if (cb_sity.Items[i].ToString() == ipInfo.City)
+                {
+                    cb_sity.SelectedIndex = i;
+                    lbl_CityName.Text = cb_sity.Items[cb_sity.SelectedIndex].ToString();
+                    break;
+                }
+            }
+
+            LoadWether(cb_sity.Items[cb_sity.SelectedIndex].ToString());
+        }
+
+        private async void LoadWether(string city)
+        {
+            string apiKey = "bcb395514086595b809ab3007e0d819a";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+            using (HttpClient client = new HttpClient()) { 
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    WeatherResponse weatherInfo = JsonConvert.DeserializeObject<WeatherResponse>(responseBody);
+
+                    int temp = Convert.ToInt32(weatherInfo.main.temp);
+                    lbl_Temp.Text = temp.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Не удалось получить информацию о погоде.");
+                }
+            }
+        }
+
+        private void cb_sity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbl_CityName.Text = cb_sity.Items[cb_sity.SelectedIndex].ToString();
+            LoadWether(cb_sity.Items[cb_sity.SelectedIndex].ToString());
+        }
+    }
+}
